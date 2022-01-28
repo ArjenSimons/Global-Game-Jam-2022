a793bcc405b641ed05715999e732cfa7846afeb0
Thijs Adema <thijsadema@hotmail.com>
2022-01-29 00:00:20 +0100

    Added keyboard input

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,29 +6,68 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
-    private Vector2 movingVector;
+    private Vector2 movingVector, keyboardInputVector;
     [SerializeField]
     private float speed;
-    private bool pressedActionButton, noInput;
+    private bool pressedActionButton, noInput, keyboardWasUsed;
 
     void Start()
     {
         pressedActionButton = false;
+        keyboardWasUsed = false;
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         var gamepad = Gamepad.current;
+        var keyboard = Keyboard.current;
 
-        if (gamepad == null) return;
+        checkGamepadInput(gamepad);
+        checkKeyBoardInput(keyboard);
+    }
 
-        if (gamepad.buttonSouth.wasPressedThisFrame)
-        {
-            pressedActionButton = true;
-        }
+    private void checkGamepadInput (Gamepad gamepad)
+    {
+        if (gamepad.buttonSouth.wasPressedThisFrame) pressedActionButton = true;
 
         movingVector = gamepad.leftStick.ReadValue() * speed;
+    }
+
+    private void checkKeyBoardInput (Keyboard keyboard)
+    {
+        keyboardInputVector = Vector2.zero;
+
+        if (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed)
+        {
+            keyboardInputVector.x -= 1;
+            keyboardWasUsed = true;
+        }
+
+        if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed)
+        {
+            keyboardInputVector.x += 1;
+            keyboardWasUsed = true;
+        }
+
+        if (keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed)
+        {
+            keyboardInputVector.y -= 1;
+            keyboardWasUsed = true;
+        }
+
+        if (keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed)
+        {
+            keyboardInputVector.y += 1;
+            keyboardWasUsed = true;
+        }
+
+        keyboardInputVector *= speed;
+
+        if (keyboardWasUsed) movingVector = keyboardInputVector;
+        keyboardWasUsed = false;
+
+        if (keyboard.spaceKey.wasPressedThisFrame) pressedActionButton = true;
     }
 
     private void FixedUpdate()
@@ -43,9 +82,6 @@ public class PlayerMovement : MonoBehaviour
 
         noInput = movingVector == Vector2.zero;
 
-        if (noInput)
-        {
-            rigidBody.velocity = Vector2.zero;
-        }
+        if (noInput) rigidBody.velocity = Vector2.zero;
     }
 }
