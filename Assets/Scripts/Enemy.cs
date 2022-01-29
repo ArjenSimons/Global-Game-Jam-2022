830bc86949945efdcdd3e2ea25dd2dfc9d96c104
Arjen Simons <arjen.simons@gmail.com>
2022-01-29 09:26:38 +0100

    Add basic flee behaviour

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,19 +5,56 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] NavMeshAgent agent;
-    [SerializeField] Transform player;
+    [Header("Settings")]
+    [SerializeField] private float fleeDistance;
+
+    [Header("References")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private Transform player;
+
+
+    private DayNightManager dayNightManager;
+    private float fleeDistanceSquared;
 
     private void Start()
     {
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        dayNightManager = DayNightManager.Instance;
 
+        fleeDistanceSquared = fleeDistance * fleeDistance;
     }
 
     private void Update()
     {
-        agent.SetDestination(player.position);
+        if (dayNightManager.CurrentDayState == DayState.DAY)
+        {
+            HandleDayBehaviour();
+        }
+        else if (dayNightManager.CurrentDayState == DayState.NIGHT)
+        {
+            HandleNightBehaviour();
+        }
 
+    }
+
+    private void HandleDayBehaviour()
+    {
+        if (Vector3.SqrMagnitude(transform.position - player.position) < fleeDistanceSquared)
+        {
+            Flee();
+        }
+    }
+
+    private void HandleNightBehaviour()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void Flee()
+    {
+        Vector3 runTo = transform.position + ((transform.position - player.position) * 3);
+
+        agent.SetDestination(runTo);
     }
 }
